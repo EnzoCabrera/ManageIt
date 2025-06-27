339085ab3fa641aad732bf00a5398e32e1ce06a6
Enzo Nascimento Cabrera <enzo2005cabrera@gmail.com>
2025-06-27 18:40:00 -0300

    Chore: Improving the code for auth endpoints

--- src/main/java/com/example/estoque/controllers/AuthController.java
+++ src/main/java/com/example/estoque/controllers/AuthController.java
@@ -7,6 +7,7 @@ import com.example.estoque.entities.User;
 import com.example.estoque.entities.UserRole;
 import com.example.estoque.infra.security.TokenService;
 import com.example.estoque.repositories.UserRepository;
+import com.example.estoque.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -25,32 +26,17 @@ public class AuthController {
     private AuthenticationManager authenticationManager;
 
     @Autowired
-    private UserRepository userRepository;
+    private AuthService authService;
 
-    @Autowired
-    private TokenService tokenService;
-
+    // Login endpoint
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody AuthDto dto){
-        var usernamePassword = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
-        var auth = this.authenticationManager.authenticate(usernamePassword);
-
-        var token = tokenService.generateToken((User) auth.getPrincipal());
-
-        return ResponseEntity.ok(new LoginResponseDto(token));
+    public ResponseEntity<LoginResponseDto> login(@RequestBody AuthDto dto) {
+        return ResponseEntity.ok(authService.login(dto, authenticationManager));
     }
 
+    // Register endpoint
     @PostMapping("/register")
-    public ResponseEntity register(@RequestBody RegisterDto dto){
-        if (this.userRepository.findByEmail(dto.email()) != null) return ResponseEntity.badRequest().build();
-
-        String encryptedPassword = new BCryptPasswordEncoder().encode(dto.password());
-        User newUser = new User(dto.email(), encryptedPassword);
-
-        newUser.setRole(UserRole.USER);
-
-        this.userRepository.save(newUser);
-
-        return ResponseEntity.ok().build();
+    public ResponseEntity<?> register(@RequestBody RegisterDto dto){
+        return authService.register(dto);
     }
 }
