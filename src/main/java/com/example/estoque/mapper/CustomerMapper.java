b92a135db8e71d1ab831f11cb6ad5f2913832beb
Enzo Nascimento Cabrera <enzo2005cabrera@gmail.com>
2025-06-28 13:57:37 -0300

    Feat: Create customers mapper

---
+++ src/main/java/com/example/estoque/mapper/CustomerMapper.java
@@ -0,0 +1,31 @@
+package com.example.estoque.mapper;
+
+import com.example.estoque.dtos.customerDtos.CustomerResponseDto;
+import com.example.estoque.entities.Customer;
+import com.example.estoque.repositories.CustomerRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerMapper {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerMapper(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public CustomerResponseDto toDto(Customer customer){
+        CustomerResponseDto dto = new CustomerResponseDto();
+        dto.setCodcus(customer.getCodcus());
+        dto.setCusname(customer.getCusname());
+        dto.setCusaddr(customer.getCusaddr());
+        dto.setCuscity(customer.getCuscity());
+        dto.setCusstate(customer.getCusstate());
+        dto.setCuszip(customer.getCuszip());
+        dto.setCusphone(customer.getCusphone());
+        dto.setCusemail(customer.getCusemail());
+        dto.setUpdatedBy(customer.getUpdatedBy());
+        dto.setUpdatedAt(customer.getUpdatedAt().toString());
+        return dto;
+    }
+}
