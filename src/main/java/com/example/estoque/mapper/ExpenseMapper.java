0e93eb9ec73684d908b758f67a3c5e4a7fc748bc
Enzo Nascimento Cabrera <enzo2005cabrera@gmail.com>
2025-06-29 08:35:29 -0300

    Feat: Create expenses mapper

---
+++ src/main/java/com/example/estoque/mapper/ExpenseMapper.java
@@ -0,0 +1,29 @@
+package com.example.estoque.mapper;
+
+import com.example.estoque.dtos.expenseDtos.ExpenseResponseDto;
+import com.example.estoque.entities.expenseEntities.Expense;
+import com.example.estoque.repositories.ExpenseRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExpenseMapper {
+
+    private final ExpenseRepository expenseRepository;
+
+    public ExpenseMapper(ExpenseRepository expenseRepository) {
+        this.expenseRepository = expenseRepository;
+    }
+
+    public ExpenseResponseDto toDto(Expense expense) {
+        ExpenseResponseDto dto = new ExpenseResponseDto();
+        dto.setCodexp(expense.getCodexp());
+        dto.setExpdesc(expense.getExpdesc());
+        dto.setExpCostInCents(expense.getExpCostInCents());
+        dto.setExpdate(expense.getExpdate());
+        dto.setExptype(expense.getExptype());
+        dto.setUpdatedBy(expense.getUpdatedBy());
+        dto.setUpdatedAt(expense.getUpdatedAt().toString());
+        return dto;
+    }
+
+}
