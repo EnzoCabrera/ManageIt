d07ed80bce27bf1acfbc53007a635041c9bf0cd7
Enzo Nascimento Cabrera <enzo2005cabrera@gmail.com>
2025-07-05 16:18:22 -0300

    Feat: Create order's items dtos

---
+++ src/main/java/com/example/estoque/dtos/itemDtos/ItemRequestDto.java
@@ -0,0 +1,25 @@
+package com.example.estoque.dtos.itemDtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ItemRequestDto {
+
+    @NotNull
+    private Long codprod;
+
+    @NotNull
+    private Integer quantity;
+
+    @NotNull
+    private Integer priceInCents;
+
+    private Float discountPercent;
+}
