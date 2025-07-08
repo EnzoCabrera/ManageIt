f1497da0ff31543dd312da6cc6301f9a018589bf
Enzo Nascimento Cabrera <enzo2005cabrera@gmail.com>
2025-07-08 20:10:00 -0300

    Feat: Add audit loggin for all Stock actions

--- src/main/java/com/example/estoque/services/StockService.java
+++ src/main/java/com/example/estoque/services/StockService.java
@@ -25,6 +25,9 @@ public class StockService {
     @Autowired
     private StockMapper stockMapper;
 
+    @Autowired
+    private AuditLogService auditLogService;
+
     //GET products logic
     public List<StockResponseDto> getFilterStock(
             String productName,
@@ -46,8 +49,22 @@ public class StockService {
         stock.setProductName(dto.getProductName());
         stock.setQuantity(dto.getQuantity());
         stock.setUnpricInCents(dto.getUnpricInCents());
+        stock.setUntype(dto.getUntype());
+        stock.setUnqtt(dto.getUnqtt());
 
         Stock save = stockRepository.save(stock);
+
+        // Log the creation of the stock item
+        auditLogService.log(
+                "Stock",
+                save.getCodProd(),
+                "CREATE",
+                null,
+                null,
+                null,
+                save.getCreatedBy()
+        );
+
         return stockMapper.toDto(save);
     }
 
@@ -56,11 +73,39 @@ public class StockService {
         Stock stock = stockRepository.findById(codProd)
                 .orElseThrow(() -> new AppException("Product not found", HttpStatus.NOT_FOUND));
 
+        // Capture old values for audit logging
+        String oldProductName = stock.getProductName();
+        Integer oldQuantity = stock.getQuantity();
+        Integer oldUnpricInCents = stock.getUnpricInCents();
+
         stock.setProductName(stockRequestDto.getProductName());
         stock.setQuantity(stockRequestDto.getQuantity());
         stock.setUnpricInCents(stockRequestDto.getUnpricInCents());
 
         Stock updatedStock = stockRepository.save(stock);
+
+        String actor = updatedStock.getUpdatedBy();
+
+        //Log the update of the stock item
+
+        //Product Name
+        if (!oldProductName.equals(updatedStock.getProductName())) {
+            auditLogService.log("Stock", updatedStock.getCodProd(), "UPDATE",
+                    "productName", oldProductName, updatedStock.getProductName(), actor);
+        }
+
+        //Quantity
+        if (!oldQuantity.equals(updatedStock.getQuantity())) {
+            auditLogService.log("Stock", updatedStock.getCodProd(), "UPDATE",
+                    "quantity", oldQuantity.toString(), updatedStock.getQuantity().toString(), actor);
+        }
+
+        //Unit Price in Cents
+        if (!oldUnpricInCents.equals(updatedStock.getUnpricInCents())) {
+            auditLogService.log("Stock", updatedStock.getCodProd(), "UPDATE",
+                    "unpricInCents", oldUnpricInCents.toString(), updatedStock.getUnpricInCents().toString(), actor);
+        }
+
         return stockMapper.toDto(updatedStock);
     }
 
@@ -71,6 +116,18 @@ public class StockService {
 
         stock.setIsDeleted(true);
         Stock updatedStock = stockRepository.save(stock);
+
+        // Log the deletion of the stock item
+        auditLogService.log(
+                "Stock",
+                updatedStock.getCodProd(),
+                "DELETE",
+                null,
+                null,
+                null,
+                updatedStock.getUpdatedBy()
+        );
+
         return stockMapper.toDto(updatedStock);
     }
 
