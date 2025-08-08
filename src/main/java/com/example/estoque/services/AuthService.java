348202f0c0f1187eac07738d531f7d8ac7878011
Enzo Nascimento Cabrera <enzo2005cabrera@gmail.com>
2025-08-08 20:00:44 -0300

    Feat(user): Creating get users endpoint logic

--- src/main/java/com/example/estoque/services/AuthService.java
+++ src/main/java/com/example/estoque/services/AuthService.java
@@ -3,12 +3,16 @@ package com.example.estoque.services;
 import com.example.estoque.dtos.authDtos.AuthDto;
 import com.example.estoque.dtos.authDtos.LoginResponseDto;
 import com.example.estoque.dtos.authDtos.RegisterDto;
+import com.example.estoque.dtos.authDtos.UserResponseDto;
 import com.example.estoque.entities.userEntities.User;
 import com.example.estoque.entities.userEntities.UserRole;
+import com.example.estoque.entities.userEntities.UserSpecification;
 import com.example.estoque.infra.security.TokenService;
+import com.example.estoque.mapper.UserMapper;
 import com.example.estoque.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,6 +21,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -29,6 +35,9 @@ public class AuthService implements UserDetailsService {
     private TokenService tokenService;
     @Autowired
     private AuditLogService auditLogService;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -76,5 +85,26 @@ public class AuthService implements UserDetailsService {
         );
 
         return ResponseEntity.ok().build();
+    }
+
+    //GET user logic
+    public List<UserResponseDto> getFilterUsers(String email, String role) {
+        UserRole roleEnum = null;
+        if (role != null && !role.isBlank()) {
+            try {
+                roleEnum = UserRole.valueOf(role.toUpperCase());
+            }   catch (IllegalArgumentException ex) {
+                return List.of();
+            }
+        }
+
+        Specification<User> spec = Specification.where
+                    (UserSpecification.hasEmail(email))
+                .and(UserSpecification.hasRole(roleEnum));
+
+        return userRepository.findAll(spec)
+                .stream()
+                .map(userMapper::toDto)
+                .toList();
     }
 }
