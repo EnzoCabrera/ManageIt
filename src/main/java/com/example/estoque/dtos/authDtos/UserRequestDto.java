7886aaae0d122c6e1e9fd1f346b347d4451c1557
Enzo Nascimento Cabrera <enzo2005cabrera@gmail.com>
2025-08-11 08:48:34 -0300

    Feat(user): Create request DTO for users

---
+++ src/main/java/com/example/estoque/dtos/authDtos/UserRequestDto.java
@@ -0,0 +1,24 @@
+package com.example.estoque.dtos.authDtos;
+
+import com.example.estoque.entities.userEntities.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserRequestDto {
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private UserRole role;
+
+    @NotNull
+    private String password;
+}
