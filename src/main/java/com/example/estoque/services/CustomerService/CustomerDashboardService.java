4e78df8549a6310ecb24883943d19f0519d4ce1c
Enzo Nascimento Cabrera <enzo2005cabrera@gmail.com>
2025-08-12 07:22:17 -0300

    feat(api): Improve GET endpoint response when there is no customer data

--- src/main/java/com/example/estoque/services/CustomerService/CustomerDashboardService.java
+++ src/main/java/com/example/estoque/services/CustomerService/CustomerDashboardService.java
@@ -1,8 +1,10 @@
 package com.example.estoque.services.CustomerService;
 
 import com.example.estoque.dtos.customerDtos.customerDashboardsDtos.*;
+import com.example.estoque.exceptions.AppException;
 import com.example.estoque.repositories.CustomerRepositories.CustomerDashboardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,21 +22,33 @@ public class CustomerDashboardService {
 
     //GET new customers per month logic
     public List<NewCusPerMonthDto> getNewCustomersPerMonth() {
+        if (customerDashboardRepository.findNewCusPerMonth().isEmpty()) {
+            throw new AppException("No new customers found for the current month.", HttpStatus.NOT_FOUND);
+        }
         return customerDashboardRepository.findNewCusPerMonth();
     }
 
     //GET top 5 customers with most orders logic
     public List<Top5CusMoreOrdDto> getTop5CusWithMostOrds() {
+        if (customerDashboardRepository.findTop5CusMoreOrd().isEmpty()) {
+            throw new AppException("There is no orders for any customer", HttpStatus.NOT_FOUND);
+        }
         return customerDashboardRepository.findTop5CusMoreOrd();
     }
 
     //GET top 5 customers with highest amount spent logic
     public List<Top5CusHighestAmountSpent> getTop5CusHighestAmountSpent() {
+        if (customerDashboardRepository.findTop5CusHighestAmountSpent().isEmpty()) {
+            throw new AppException("any customer has bought", HttpStatus.NOT_FOUND);
+        }
         return customerDashboardRepository.findTop5CusHighestAmountSpent();
     }
 
     //GET inactive customers logic
     public List<InactiveCustomerDto> getInactiveCustomers() {
+        if (customerDashboardRepository.findInactiveCustomers().isEmpty()) {
+            throw new AppException("Any customer is inactive", HttpStatus.NOT_FOUND);
+        }
         return customerDashboardRepository.findInactiveCustomers();
     }
 }
