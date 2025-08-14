cd5891775689916eb3134e208fb5a36cc90db979
Enzo Nascimento Cabrera <enzo2005cabrera@gmail.com>
2025-08-14 07:52:31 -0300

    feat(user): Create page dto to organize and simplify de response

---
+++ src/main/java/com/example/estoque/dtos/authDtos/PageResponseDto.java
@@ -0,0 +1,23 @@
+package com.example.estoque.dtos.authDtos;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResponseDto<T> {
+    private List<T> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+    private boolean first;
+    private boolean last;
+
+}
