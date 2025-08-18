8f6f1a7432cb800751d2be429e6471b779ec6ae4
Enzo Nascimento Cabrera <enzo2005cabrera@gmail.com>
2025-08-18 06:42:42 -0300

    feat(log): Adjust service to use pageable

--- src/main/java/com/example/estoque/services/AuditLogService.java
+++ src/main/java/com/example/estoque/services/AuditLogService.java
@@ -1,12 +1,22 @@
 package com.example.estoque.services;
 
+import com.example.estoque.dtos.AuditLogResponseDto;
+import com.example.estoque.dtos.authDtos.PageResponseDto;
 import com.example.estoque.entities.LogEntities.AuditLog;
+import com.example.estoque.entities.LogEntities.AuditLogSpecification;
 import com.example.estoque.exceptions.AppException;
+import com.example.estoque.mapper.AuditLogMapper;
 import com.example.estoque.repositories.AuditLogRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -15,14 +25,47 @@ public class AuditLogService {
 
     private final AuditLogRepository auditLogRepository;
 
-    public List<AuditLog> getLogsByEntity(String entity) {
+    @Autowired
+    private AuditLogMapper auditLogMapper;
 
-        if (entity == null || entity.isEmpty()) {
-            throw new AppException("No users found matching the given filters.", HttpStatus.NOT_FOUND);
+    //GET logs logic
+    public PageResponseDto<AuditLogResponseDto> getLogs(
+            String entity,
+            String action,
+            String changedBy,
+            LocalDateTime from,
+            LocalDateTime to,
+            Pageable pageable) {
+
+        Specification<AuditLog> spec = Specification
+                .where(AuditLogSpecification.hasEntity(entity))
+                .and(AuditLogSpecification.hasAction(action))
+                .and(AuditLogSpecification.hasChangedBy(changedBy))
+                .and(AuditLogSpecification.createdBetween(from, to));
+
+        Page<AuditLog> page = auditLogRepository.findAll(spec, pageable);
+
+        if (page.isEmpty()) {
+            throw new AppException("No logs found for the given filters", HttpStatus.NOT_FOUND);
         }
-        return auditLogRepository.findByEntity(entity.toUpperCase());
+
+        List<AuditLogResponseDto> content = page.getContent()
+                .stream()
+                .map(auditLogMapper::toDto)
+                .toList();
+
+        return new PageResponseDto<>(
+                content,
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements(),
+                page.getTotalPages(),
+                page.isFirst(),
+                page.isLast()
+        );
     }
 
+    //Log registration logic
     public void log(String entity, Long entityId, String action, String field, String oldValue, String newValue, String changedBy) {
         AuditLog log = new AuditLog();
         log.setEntity(entity.toUpperCase());
