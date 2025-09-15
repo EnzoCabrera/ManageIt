dde551410a742d62612510d92908d0a795226525
Enzo Nascimento Cabrera <enzo2005cabrera@gmail.com>
2025-09-15 09:45:07 -0300

    feat(test): Add test for customer not found

---
+++ src/test/java/com/example/estoque/services/OrderServices/OrderServiceTest.java
@@ -0,0 +1,62 @@
+package com.example.estoque.services.OrderServices;
+
+import com.example.estoque.dtos.itemDtos.ItemRequestDto;
+import com.example.estoque.dtos.orderDtos.OrderRequestDto;
+import com.example.estoque.entities.OrderEntities.OrderStatus;
+import com.example.estoque.entities.customerEntities.Customer;
+import com.example.estoque.entities.stockEntities.Stock;
+import com.example.estoque.entities.stockEntities.StockUnitType;
+import com.example.estoque.exceptions.AppException;
+import com.example.estoque.mapper.OrderMapper;
+import com.example.estoque.repositories.CustomerRepositories.CustomerRepository;
+import com.example.estoque.repositories.ItemRepository;
+import com.example.estoque.repositories.OrderRepositories.OrderRepository;
+import com.example.estoque.repositories.StockRepository;
+import com.example.estoque.services.AuditLogService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderServiceTest {
+
+
+
+    @Mock private CustomerRepository customerRepository;
+    @Mock private StockRepository stockRepository;
+    @Mock private OrderRepository orderRepository;
+    @Mock private ItemRepository itemRepository;
+    @Mock private OrderNotificationService orderNotificationService;
+    @Mock private AuditLogService auditLogService;
+    @Mock private OrderMapper orderMapper;
+
+    @InjectMocks
+    private OrderService orderService;
+
+
+    //Customer not found test
+    @Test
+    void shouldThrowExceptionWhenCustomerNotFound() {
+        OrderRequestDto dto = new OrderRequestDto();
+        dto.setCodcus(1L);
+        dto.setItems(new ArrayList<>());
+
+        when(customerRepository.findBycodcusAndIsDeletedFalse(1L))
+                .thenReturn(Optional.empty());
+
+        AppException ex = assertThrows(AppException.class, () -> orderService.registerOrder(dto));
+        assertEquals("Customer not found or disabled", ex.getMessage());
+    }
+
+}
